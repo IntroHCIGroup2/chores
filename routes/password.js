3f9f58952e0e5da225bb404a91770dbef7ad0aca
Cesar Cisneros <cecisner@ucsd.edu>
2014-02-26 18:05:01 -0800

    git fixes

--- routes/password.js
+++ routes/password.js
@@ -1,15 +1,7 @@
 // Get all of our friend data
-var data = require('../data.json'); 
+var data = require('../data.json');
 
-exports.changePassword = function(req, res){
-	var oldPass = req.query.oldpass;
-    var newPass = req.query.newpass;
-    
-    console.log(oldPass);
-    console.log(newPass);
-    
-    
-    
+exports.view = function(req, res){
 	res.render('password', data);
 }
 
@@ -19,22 +11,13 @@ exports.changePassword = function(req, res) {
 	var roommateID = req.session.roommateID;
 	var newpassword = req.query.newpassword;
 	var oldpassword = req.query.oldpassword;
-<<<<<<< HEAD
     
-    console.log(newpassword);
-    console.log(oldpassword);
-    
-    
+    //    if(oldpassword == data.Roommates[roommateID].password){
             data.Roommates[roommateID].password = newpassword;
-            console.log(data.Roommates[roommateID].password);
-        /*}
+   /*     }
         else{
             //console log error message
         }*/
-=======
-	console.log("pw1 " + newpassword + " pw 2 " + oldpassword);
-		data.Roommates[roommateID].password = newpassword;
->>>>>>> f9f03b699d17f3db16adaa80eb069311a6e95d39
         res.render('password',data);
 
 }
